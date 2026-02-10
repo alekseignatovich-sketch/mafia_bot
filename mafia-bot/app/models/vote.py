c17bba383639afad9a08afcbdca0ff6bfff6c329
alekseignatovich-sketch <alekseignatovich@gmail.com>
2026-02-10 15:30:23 +0300

    Update vote.py

--- mafia-bot/app/models/vote.py
+++ mafia-bot/app/models/vote.py
@@ -3,7 +3,7 @@
 from datetime import datetime
 from typing import TYPE_CHECKING, Optional
 
-from sqlalchemy import BigInteger, Boolean, DateTime, ForeignKey, Integer
+from sqlalchemy import DateTime, BigInteger, Boolean, DateTime, ForeignKey, Integer
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.models.base import Base
